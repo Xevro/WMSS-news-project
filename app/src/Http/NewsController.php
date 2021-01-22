91beded72c8eba0e962a974b33853a69d8395299
Louis D'Hont <louisdhont@MacBook-Pro-15-van-Louis.local>
2021-01-22 14:23:40 +0100

    adding articles to homepage

--- app/src/Http/NewsController.php
+++ app/src/Http/NewsController.php
@@ -17,6 +17,8 @@ class NewsController {
     }
 
     public function home() {
-        echo $this->twig->render('pages/home.twig', []);
+        $articles = $this->db->fetchAllAssociative('SELECT n.id, n.title, n.message, n.pubdate, n.alt, n.popularity, a.firstname as author FROM `newsmessages` as n LEFT JOIN authors as a on n.author_id = a.id ORDER BY popularity DESC LIMIT 3;', []);
+        $latestArticle = $this->db->fetchAssociative('SELECT n.id, n.title, n.message, n.pubdate, n.alt, n.popularity, a.firstname as author FROM `newsmessages` as n LEFT JOIN authors as a on n.author_id = a.id ORDER BY `pubdate` DESC LIMIT 1;', []);
+        echo $this->twig->render('pages/home.twig', ['articles' => $articles, 'latest' => $latestArticle]);
     }
 }
