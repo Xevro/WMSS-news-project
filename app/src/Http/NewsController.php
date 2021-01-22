f0eae278e60d9be007ce2dc71e54aa194fcaa228
Louis D'Hont <louisdhont@MacBook-Pro-15-van-Louis.local>
2021-01-22 14:33:19 +0100

    adding categories to homepage

--- app/src/Http/NewsController.php
+++ app/src/Http/NewsController.php
@@ -7,6 +7,7 @@ use Services\DatabaseConnector;
 class NewsController {
     protected \Doctrine\DBAL\Connection $db;
     protected \Twig\Environment $twig;
+    protected array $categories;
 
     public function __construct() {
         // bootstrap Twig
@@ -14,11 +15,12 @@ class NewsController {
         $this->twig = new \Twig\Environment($loader);
         //Database connection
         $this->db = DatabaseConnector::getConnection();
+        $this->categories = $this->db->fetchAllAssociative('SELECT * FROM categories ORDER BY id', []);
     }
 
     public function home() {
         $articles = $this->db->fetchAllAssociative('SELECT n.id, n.title, LEFT(n.message, 250) as message, n.pubdate, n.alt, n.popularity, a.firstname as author FROM `newsmessages` as n LEFT JOIN authors as a on n.author_id = a.id ORDER BY popularity DESC LIMIT 3;', []);
         $latestArticle = $this->db->fetchAssociative('SELECT n.id, n.title,LEFT(n.message, 250) as message, n.pubdate, n.alt, n.popularity, a.firstname as author FROM `newsmessages` as n LEFT JOIN authors as a on n.author_id = a.id ORDER BY `pubdate` DESC LIMIT 1;', []);
-        echo $this->twig->render('pages/home.twig', ['articles' => $articles, 'latest' => $latestArticle]);
+        echo $this->twig->render('pages/home.twig', ['articles' => $articles, 'latest' => $latestArticle, 'categories' => $this->categories]);
     }
 }
